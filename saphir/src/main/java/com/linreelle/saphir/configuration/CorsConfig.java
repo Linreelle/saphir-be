90b4e116a5076ffb232b66b0149359ff2a0e1cad
Linreelle <116360219+Linreelle@users.noreply.github.com>
2025-07-31 13:48:15 -0700

    Update CorsConfig.java

--- saphir/src/main/java/com/linreelle/saphir/configuration/CorsConfig.java
+++ saphir/src/main/java/com/linreelle/saphir/configuration/CorsConfig.java
@@ -1,18 +1,18 @@
-package com.linreelle.saphir.configuration;
+// package com.linreelle.saphir.configuration;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class CorsConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("https://linreelle.github.io")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
-                .allowCredentials(true);
-    }
+// @Configuration
+// public class CorsConfig implements WebMvcConfigurer {
+//     @Override
+//     public void addCorsMappings(CorsRegistry registry) {
+//         registry.addMapping("/**")
+//                 .allowedOrigins("https://linreelle.github.io")
+//                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+//                 .allowCredentials(true);
+//     }
 
 
-}
+// }
