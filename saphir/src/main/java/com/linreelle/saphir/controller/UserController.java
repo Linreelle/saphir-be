e6ca46aaed535cc86e2baab5f1bd4aba7128c22e
Linreelle <116360219+Linreelle@users.noreply.github.com>
2025-07-31 13:45:31 -0700

    Update UserController.java

--- saphir/src/main/java/com/linreelle/saphir/controller/UserController.java
+++ saphir/src/main/java/com/linreelle/saphir/controller/UserController.java
@@ -28,8 +28,6 @@ import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
-@CrossOrigin(origins = "https://linreelle.github.io", allowCredentials = "true")
-
 @Slf4j
 @RestController
 @RequiredArgsConstructor
